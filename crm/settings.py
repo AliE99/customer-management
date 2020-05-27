65c0630700ad403204190188db8f6ca365961c6f
AliE99 <alielahi78@yahoo.com>
2020-05-28 01:52:45 +0430

    Static folder inserted

--- crm/settings.py
+++ crm/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'accounts',
 ]
 
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
